e9905144c8f6106cfa71b6afa64a5761db6feb83
EC2 Default User <ec2-user@ip-172-31-9-180.ap-northeast-1.compute.internal>
2023-11-07 15:59:13 +0000

    いいねの非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,22 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-     comment.save
-    redirect_to book_path(book)
+    comment.book_id = @book.id
+    comment.save
+    # book_path(book)
+    @book_comment=BookComment.new
+    render "comment"
   end
   
   def destroy
-    is_matching_login_user
-    current_user.book_comments.find(params[:id]).destroy!
-    flash[:notice] = 'コメントを削除しました'
-    redirect_to("/books/#{params[:book_id]}")
+    @book_comment = BookComment.find(params[:id])
+    @book = @book_comment.book
+    @book_comment.destroy
+    # flash[:notice] = 'コメントを削除しました'
+    @book_comment=BookComment.new
+    # ("/books/#{params[:book_id]}")
+    render "comment"
   end
 
   private
@@ -20,11 +25,11 @@ class BookCommentsController < ApplicationController
     params.require(:book_comment).permit(:comment)
   end
   
-  def is_matching_login_user
-    book_comment = BookComment.find(params[:id])
-    unless book_comment.user.id == current_user.id
-      redirect_to books_path
-    end
-  end
+  # def is_matching_login_user
+  #   book_comment = BookComment.find(params[:id])
+  #   unless book_comment.user.id == current_user.id
+  #     # books_path
+  #   end
+  # end
 
 end
